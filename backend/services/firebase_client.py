7a1c3744560e565e32b44cf17d047e7c6f5eabf3
sarasinghrse <20singhsara@gmail.com>
2026-04-24 01:57:01 +0530

    fix firebase client for Render deployment

--- backend/services/firebase_client.py
+++ backend/services/firebase_client.py
@@ -1,7 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+cred_dict = json.loads(cred_json)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
