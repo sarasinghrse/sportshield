b42057e094b194a66895e65ee3e58827a78290a2
sarasinghrse <20singhsara@gmail.com>
2026-04-26 02:17:18 +0530

    fix: sportshield favicon

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import '../styles/sportshield.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/sportshield-logo-transparent.png" />
+        <link rel="apple-touch-icon" href="/images/sportshield-logo-transparent.png" />
+        <meta name="theme-color" content="#0a1210" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
